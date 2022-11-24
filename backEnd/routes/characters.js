752b6997eed14e159b3b4bd5040388fdc77eaddf
Craig Hetherington <92098558+TheTrueDarky@users.noreply.github.com>
2022-11-24 13:40:51 +0000

    edits

--- backEnd/routes/characters.js
+++ backEnd/routes/characters.js
@@ -6,4 +6,8 @@ router.get('/', controller.getAll);
 router.get('/desc/:value', controller.getByDesc);
 router.get('/:id', controller.getById);
 
+router.post('/', controller.create);
+router.delete('/',controller.deleting);
+router.put('/', controller.update);
+
 module.exports = router;
