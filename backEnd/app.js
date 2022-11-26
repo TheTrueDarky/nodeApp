78e3b7917ed44eff630448b16bed1a8c318a444e
Craig Hetherington <92098558+TheTrueDarky@users.noreply.github.com>
2022-11-26 18:01:25 +0000

    added missing files

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const logger = require('morgan');
-const toolsRouter = require('./routes/characters');
+const charactersRouter = require('./routes/characters');
+const linksRouter = require('./routes/link');
 
 const app = express();
 app.use(express.json());
 app.use(logger('dev'));
 
 app.use("/characters", charactersRouter);
+app.use("/link",linksRouter);
 
 app.use((req, res) =>
  res.status(404).send("Sorry page not found!"));
