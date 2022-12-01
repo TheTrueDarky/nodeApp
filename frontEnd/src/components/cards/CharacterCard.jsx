97b001f3737a33db2805bead89910c6ecd5a6f09
Craig Hetherington <92098558+TheTrueDarky@users.noreply.github.com>
2022-12-01 13:05:46 +0000

    edit

--- frontEnd/src/components/cards/CharacterCard.jsx
+++ frontEnd/src/components/cards/CharacterCard.jsx
@@ -11,7 +11,7 @@ function CharacterCard(props) {
         <Card key={props.id}>
             <Card.Body>
                 <Card.Title>{props.first_name}</Card.Title>
-                <Card.Text>Surname: £{props.surname}</Card.Text>
+                <Card.Text>Surname: {props.surname}</Card.Text>
                 <Card.Text>Gender: {props.gender}</Card.Text>
                 <Card.Text>Age: {props.age}</Card.Text>
                 <Card.Text>Deaths: {props.deaths}</Card.Text>
