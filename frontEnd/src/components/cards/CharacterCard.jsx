508e8969410b913536173c12af6afeb46d2c822b
Craig Hetherington <92098558+TheTrueDarky@users.noreply.github.com>
2022-12-16 22:13:21 +0000

    edits to css

--- frontEnd/src/components/cards/CharacterCard.jsx
+++ frontEnd/src/components/cards/CharacterCard.jsx
@@ -33,7 +33,7 @@ function CharacterCard(props) {
 
     console.log(props);
     return (
-        <Card style={{overflow: "hidden", maxHeight: "600px"}} key={props.id} >
+        <Card style={{overflow: "hidden", maxHeight: "600px", border: "1px solid black", borderStyle: "double"}} key={props.id} >
             <Card.Body>
                 <Card.Title>{props.first_name}</Card.Title>
                 <Card.Text>Surname: {props.surname}</Card.Text>
