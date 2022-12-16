f877783f04414ab8570b00a89dfb4eccb8c610a1
Craig Hetherington <92098558+TheTrueDarky@users.noreply.github.com>
2022-12-16 19:19:21 +0000

    added search function

--- frontEnd/src/pages/ViewCharacters.js
+++ frontEnd/src/pages/ViewCharacters.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react';
 import { Container, Alert } from 'react-bootstrap';
 import './ViewCharacters.css';
+import './search.css';
 import { getCharacter } from '../actions/actions';
 import CharacterCard from '../components/cards/CharacterCard';
 
 function ViewCharacters() {
     const [characters, setCharacters] = useState([]);
     const [error, setError] = useState([null]);
+    const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
-        if (characters.length <= 0){
+        if (characters.length <= 0) {
             const fetchData = async () => {
                 try {
                     let data = await getCharacter();
@@ -23,30 +25,34 @@ function ViewCharacters() {
             fetchData();
         }
     }, [characters])
+    const filteredCharacters = characters.filter(character => character.first_name.toLowerCase().includes(searchQuery.toLowerCase()));
 
-    if (characters.length > 0){
+    if (characters.length > 0) {
         return (
             <div className='view-characters'>
                 <Container>
                     <h1>View Characters</h1>
+                    <div className="search-box">
+                        <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+                    </div>
                     <div className="character-card-grid">
                         {
-                            characters.map((character) => {
-                                return <CharacterCard 
-                                key={character.id}
-                                id={character.id}
-                                first_name={character.first_name}
-                                surname={character.surname}
-                                gender={character.gender}
-                                age={character.age}
-                                deaths={character.deaths}
-                                character_type={character.character_type}
-                                link_id={character.link_id}
-                                
-                                demon_type={"TESTING"}
-                                auth_notes={character.auth_notes}
-                                comments={character.comments}
-                                
+                            filteredCharacters.map((character) => {
+                                return <CharacterCard
+                                    key={character.id}
+                                    id={character.id}
+                                    first_name={character.first_name}
+                                    surname={character.surname}
+                                    gender={character.gender}
+                                    age={character.age}
+                                    deaths={character.deaths}
+                                    character_type={character.character_type}
+                                    link_id={character.link_id}
+
+                                    demon_type={"TESTING"}
+                                    auth_notes={character.auth_notes}
+                                    comments={character.comments}
+
                                 />
                             })
                         }
@@ -55,14 +61,14 @@ function ViewCharacters() {
             </div>
         );
     }
-    else if (error || characters.length === 0){
+    else if (error || characters.length === 0) {
         return (
             <div className='view-characters'>
                 <Container>
                     <h1>View Characters</h1>
                     <Alert variant='danger'>
                         <Alert.Heading> An error has Occured </Alert.Heading>
-                        <p>{(error !== null ) ? error: "you currently hae no characters avaliable in your service."}  </p>
+                        <p>{(error !== null) ? error : "you currently hae no characters avaliable in your service."}  </p>
                     </Alert>
                 </Container>
             </div>
