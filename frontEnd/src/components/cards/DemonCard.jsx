2b4baa3562021cb2e69ee6ffbc7fe2c2f15c4337
Craig Hetherington <92098558+TheTrueDarky@users.noreply.github.com>
2022-12-21 18:28:02 +0000

    edits

--- frontEnd/src/components/cards/DemonCard.jsx
+++ frontEnd/src/components/cards/DemonCard.jsx
@@ -10,12 +10,22 @@ function DemonCard(props) {
     return (
         <Card key={props.id}>
             <Card.Body>
-                <Card.Title>{props.first_name}</Card.Title>
+                <Card.Title>{props.character}</Card.Title>
+                <Card.Text>Surname: {props.surname}</Card.Text>
+                <Card.Text>Gender: {props.gender}</Card.Text>
+                <Card.Text>Age: {props.age}</Card.Text>
+                <Card.Text>Deaths: {props.deaths}</Card.Text>
+                <Card.Text>Character type: {props.character_type}</Card.Text>
+                <Card.Text>link id: {props.link_id}</Card.Text>
+
                 <Card.Text>Demon Type: {props.demon_type}</Card.Text>
                 <Card.Text>Ability: {props.ability}</Card.Text>
+
+                <Card.Text>Author notes: {props.auth_notes}</Card.Text>
+                <Card.Text>Comments: {props.comments}</Card.Text>
             </Card.Body>
         </Card>
-    )
+    );
 }
 
 export default DemonCard;
