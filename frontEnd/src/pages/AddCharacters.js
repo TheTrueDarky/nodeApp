a4116bd9aa201139451fb8715f0b47b2e14475c3
Craig Hetherington <92098558+TheTrueDarky@users.noreply.github.com>
2023-01-04 15:11:05 +0000

    edits

--- frontEnd/src/pages/AddCharacters.js
+++ frontEnd/src/pages/AddCharacters.js
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import { Form, Container, Button, Alert } from 'react-bootstrap';
-import {addCharacter} from '../actions/actions';
+import { addCharacter } from '../actions/actions';
 import './AddCharacters.css';
-
 
 function AddCharacters() {
     const [first_name, setFirst_Name] = useState('');
@@ -14,17 +13,20 @@ function AddCharacters() {
     const [auth_notes, setAuth_Notes] = useState('');
     const [comments, setComments] = useState('');
     const [error, setError] = useState(null);
-    const [success, setSuccess] = useState(false);
+    const [showSuccess, setShowSuccess] = useState(false);
 
-
-    
     const submitData = async (character) => {
         try {
             let response = await addCharacter(character);
 
             if (response) {
-                setSuccess(true);
-                console.log("Yoda");
+                setShowSuccess(true);
+                let addAnother = window.confirm("Would you like to add another character?");
+                if (addAnother) {
+                    window.location.reload();
+                } else {
+                    window.location.assign("/viewchar");
+                }
             }
         }
         catch (e) {
@@ -35,7 +37,7 @@ function AddCharacters() {
     const submitCharacter = (e) => {
         e.preventDefault();
 
-        setSuccess(false);
+        setShowSuccess(false);
         setError('');
 
         if (first_name && gender && age && deaths && character_type) {
@@ -51,9 +53,8 @@ function AddCharacters() {
                 auth_notes: auth_notes,
                 comments: comments
             };
-            
 
-            if(nullFields == 0){
+            if (nullFields == 0) {
                 submitData(character);
             }
         }
@@ -61,111 +62,107 @@ function AddCharacters() {
         else {
             setError('All fields must contain a value');
         }
-
-        <Container>
-            <Alert show={success} variant="success">
-                <Alert.Heading>Success!</Alert.Heading>
-                <p>You have successfully added a new character to the service!</p>
-            </Alert>
-            <Alert show={(error !== '') ? false : true} variant="danger">
-                <Alert.Heading>An Error has Occured!</Alert.Heading>
-                <p>{error}</p>
-            </Alert>
-        </Container> 
-
     }
+
+
     return (
         <div className='add-characters'>
             <h1>Add Characters</h1>
+            {showSuccess && (
+                <Container>
+                    <Alert show={showSuccess} variant="success">
+                        <Alert.Heading>Success!</Alert.Heading>
+                        <p>You have successfully added a new character to the service!</p>
+                    </Alert>
+                </Container>
+            )}
             <Container>
                 <Form onSubmit={submitCharacter}>
                     <Form.Group className='form-group' controlId='first_name'>
                         <label>First name:</label>
-                        <Form.Control 
+                        <Form.Control
                             type='text'
                             placeholder='Enter First Name'
                             onChange={e => setFirst_Name(e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1))}
-                            required/>
+                            required />
                     </Form.Group>
                     <Form.Group className='form-group' controlId='surname'>
                         <label>Surname:</label>
-                        <Form.Control 
+                        <Form.Control
                             type='text'
                             placeholder='Enter Surname'
                             onChange={e => setSurname(e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1))}
-                            />
+                        />
                     </Form.Group>
                     <Form.Group className='form-group' controlId='gender'>
                         <label>Gender:</label>
-                            <Form.Select style={{ width: "75%", height: "45px" }} aria-label="Default select example" onChange={e => setGender(e.target.value)}
+                        <Form.Select style={{ width: "75%", height: "45px" }} aria-label="Default select example" onChange={e => setGender(e.target.value)}
                             required>
-                                <option>Choose Gender</option>
-                                <option value="Male">Male</option>
-                                <option value="Female">Female</option>
-                                <option value="Other">Other</option>
-                            </Form.Select>
-                            
+                            <option>Choose Gender</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                            <option value="Other">Other</option>
+                        </Form.Select>
                     </Form.Group>
                     <Form.Group className='form-group' controlId='age'>
                         <label>Age:</label>
-                        <Form.Control 
+                        <Form.Control
                             type='number'
-                            step='1'
-                            placeholder='0'
+                            min='0'
+                            placeholder='Enter Age'
                             onChange={e => setAge(e.target.value)}
-                            required/>
+                            required />
                     </Form.Group>
                     <Form.Group className='form-group' controlId='deaths'>
-                        <label>Deaths:</label>
-                        <Form.Control 
+                        <label>Number of Deaths:</label>
+                        <Form.Control
                             type='number'
-                            step='1'
-                            placeholder='0'
+                            min='0'
+                            placeholder='Enter Number of Deaths'
                             onChange={e => setDeaths(e.target.value)}
-                            required/>
+                            required />
                     </Form.Group>
                     <Form.Group className='form-group' controlId='character_type'>
-                        <label>Character type:</label>
-                            <Form.Select style={{ width: "75%", height: "45px" }} aria-label="Default select example" onChange={e => setCharacter_Type(e.target.value)}
+                        <label>Character Type:</label>
+                        <Form.Select style={{ width: "75%", height: "45px" }} aria-label="Default select example" onChange={e => setCharacter_Type(e.target.value)}
                             required>
-                                <option>Choose type of character</option>
-                                <option value="Demon">Demon</option>
-                                <option value="Giant">Giant</option>
-                                <option value="God">God</option>
-                                <option value="Goddess">Goddess</option>
-                                <option value="Monster">Monster</option>
-                                <option value="Mortal">Mortal</option>
-                                <option value="Primordial">Primordial</option>
-                                <option value="Titan">Titan</option>
-                                <option value="Titaness">Titaness</option>
-                                <option value="Demi-god">Demi-god</option>
-                            </Form.Select>
-                            
-                           
+                            <option>Choose type of character</option>
+                            <option value="Demon">Demon</option>
+                            <option value="Giant">Giant</option>
+                            <option value="God">God</option>
+                            <option value="Goddess">Goddess</option>
+                            <option value="Monster">Monster</option>
+                            <option value="Mortal">Mortal</option>
+                            <option value="Primordial">Primordial</option>
+                            <option value="Titan">Titan</option>
+                            <option value="Titaness">Titaness</option>
+                            <option value="Demi-god">Demi-god</option>
+                        </Form.Select>
                     </Form.Group>
                     <Form.Group className='form-group' controlId='auth_notes'>
-                        <label>Author notes:</label>
-                        <Form.Control 
-                            type='text'
-                            placeholder='Enter author notes'
+                        <label>Author Notes:</label>
+                        <Form.Control
+                            as='textarea'
+                            rows='3'
+                            placeholder='Enter Author Notes'
                             onChange={e => setAuth_Notes(e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1))}
-                            />
+                        />
                     </Form.Group>
                     <Form.Group className='form-group' controlId='comments'>
                         <label>Comments:</label>
-                        <Form.Control 
-                            type='text'
-                            placeholder='Enter comments'
+                        <Form.Control
+                            as='textarea'
+                            rows='3'
+                            placeholder='Enter Comments'
                             onChange={e => setComments(e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1))}
-                            />
+                        />
                     </Form.Group>
                     <Button variant='primary' type='submit'>
-                        Add Character
+                        Submit
                     </Button>
                 </Form>
             </Container>
         </div>
     );
 }
-
 export default AddCharacters;
